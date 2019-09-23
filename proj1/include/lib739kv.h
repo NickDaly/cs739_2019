ce883bb2573e1520926e17501f4f4ea0aae8fd07
Sek Cheong <scheong@gmail.com>
2019-09-23 13:59:14 -0500

    update

--- proj1/include/lib739kv.h
+++ proj1/include/lib739kv.h
@@ -1,10 +1,25 @@
 #ifndef LIB739KB_H
 #define LIB739KB_H
 
-
+//Provide a null-terminated array of server names (similarly to argv[]). 
+//Each server name has the format "host:port" and initialize the client code. 
+//Returns 0 on success and -1 on failure. 
 int kv739_init(char ** server_list);
+
+//Shutdown the connection to a server and free state.
+//After calling this, client code should be able to call kv739_init() again to the same or a different server. 
 int kv739_shutdown(void);
+
+//Retrieve the value corresponding to the key. 
+//If the key is present, it should return 0 and store the value in the provided string. 
+//The string must be at least 1 byte larger than the maximum allowed value. If the key is not present, it should return 1. 
+//If there is a failure, it should return -1. 
 int kv739_get(char * key, char * value);
+
+
+//Perform a get operation on the current value into old_value and then store the specified value. 
+//Should return 0 on success if there is an old value, 1 on success if there was no old value, and -1 on failure. 
+//The old_value parameter must be at least one byte larger than the maximum value size. 
 int kv739_put(char * key, char * value, char * old_value);
 
 
---
+++ proj1/src/kvservice.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <syslog.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+#include "debug.h"
+#include "exception.h"
+#include "server.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+int main(int argc, char *argv[]) {
+	
+}
