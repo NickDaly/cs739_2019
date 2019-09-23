f2fc655ea58f1975db87933d78f572f9113b463c
Sek Cheong <scheong@gmail.com>
2019-09-23 02:59:25 -0500

    fix daemon

--- proj1/src/kvdaemon.cpp
+++ proj1/src/kvdaemon.cpp
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <sys/stat.h>
+
 #include "debug.h"
 #include "exception.h"
 #include "server.h"
@@ -16,90 +17,90 @@
 #include "stdio.h"
 
 
-void daemonize(const char *cmd) {
-	int i, fd0, fd1, fd2;
-	pid_t pid;
-	struct rlimit rl;
-	struct sigaction sa;
+// void daemonize(const char *cmd) {
+// 	int i, fd0, fd1, fd2;
+// 	pid_t pid;
+// 	struct rlimit rl;
+// 	struct sigaction sa;
 
-	//Clear file creation mask.
-	umask(0);
+// 	//Clear file creation mask.
+// 	umask(0);
 
-	// Get maximum number of file descriptors.
-	if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
-		DEBUG_PRINT("%s: can’t get file limit", cmd);
-		exit(-1);
-	}
+// 	// Get maximum number of file descriptors.
+// 	if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+// 		DEBUG_PRINT("%s: can’t get file limit", cmd);
+// 		exit(-1);
+// 	}
 
- 	//Become a session leader to lose controlling TTY.
-	if ((pid = fork()) < 0) {
-		DEBUG_PRINT("%s: can’t fork", cmd);
-		exit(-1);
-	}
-	else if (pid != 0) {
-		exit(0);
-	}
+//  	//Become a session leader to lose controlling TTY.
+// 	if ((pid = fork()) < 0) {
+// 		DEBUG_PRINT("%s: can’t fork", cmd);
+// 		exit(-1);
+// 	}
+// 	else if (pid != 0) {
+// 		exit(0);
+// 	}
 
-	setsid();
-	//Ensure future opens won’t allocate controlling TTYs.
+// 	setsid();
+// 	//Ensure future opens won’t allocate controlling TTYs.
  
-	sa.sa_handler = SIG_IGN;
-	sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
+// 	sa.sa_handler = SIG_IGN;
+// 	sigemptyset(&sa.sa_mask);
+//     sa.sa_flags = 0;
     
-    if (sigaction(SIGHUP, &sa, NULL) < 0) {
-        DEBUG_PRINT("%s: can’t ignore SIGHUP", cmd);
-        exit(-1);
-    }
+//     if (sigaction(SIGHUP, &sa, NULL) < 0) {
+//         DEBUG_PRINT("%s: can’t ignore SIGHUP", cmd);
+//         exit(-1);
+//     }
 
-    if ((pid = fork()) < 0) {
-        DEBUG_PRINT("%s: can’t fork", cmd);
-        exit(-1);
-    }
-    else if (pid != 0)  {
-		exit(0);
-	}
+//     if ((pid = fork()) < 0) {
+//         DEBUG_PRINT("%s: can’t fork", cmd);
+//         exit(-1);
+//     }
+//     else if (pid != 0)  {
+// 		exit(0);
+// 	}
     
-	// Change the current working directory to the root so
-	// we won’t prevent file systems from being unmounted.
-    if (chdir("/") < 0) {
-        DEBUG_PRINT("%s: can’t change directory to /", cmd);
-        exit(-1);
-    }
+// 	// Change the current working directory to the root so
+// 	// we won’t prevent file systems from being unmounted.
+//     if (chdir("/") < 0) {
+//         DEBUG_PRINT("%s: can’t change directory to /", cmd);
+//         exit(-1);
+//     }
 
-    //Close all open file descriptors.
-    if (rl.rlim_max == RLIM_INFINITY) {
-        rl.rlim_max = 1024;
-    }
+//     //Close all open file descriptors.
+//     if (rl.rlim_max == RLIM_INFINITY) {
+//         rl.rlim_max = 1024;
+//     }
 
-    for (i = 0; i < rl.rlim_max; i++) {
-        close(i);
-    }
+//     for (i = 0; i < rl.rlim_max; i++) {
+//         close(i);
+//     }
     
 
-    //attach file descriptors 0, 1, and 2 to /dev/null
-    fd0 = open("/dev/null", O_RDWR);
-    fd1 = dup(0);
-    fd2 = dup(0);
+//     //attach file descriptors 0, 1, and 2 to /dev/null
+//     fd0 = open("/dev/null", O_RDWR);
+//     fd1 = dup(0);
+//     fd2 = dup(0);
     
-    //Initialize the log file
-    openlog(cmd, LOG_CONS, LOG_DAEMON);
-    if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
-        syslog(LOG_ERR, "unexpected file descriptors %d %d %d", fd0, fd1, fd2);
-		exit(1); 
-	}
-}
+//     //Initialize the log file
+//     openlog(cmd, LOG_CONS, LOG_DAEMON);
+//     if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
+//         syslog(LOG_ERR, "unexpected file descriptors %d %d %d", fd0, fd1, fd2);
+// 		exit(1); 
+// 	}
+// }
 
 
-void sigterm(int signo) {
-    syslog(LOG_INFO, "got SIGTERM; exiting");
-	exit(0); 
-}
+// void sigterm(int signo) {
+//     syslog(LOG_INFO, "got SIGTERM; exiting");
+// 	exit(0); 
+// }
 
 
-void sighup(int signo) {
-    syslog(LOG_INFO, "Re-reading configuration file");
-}
+// void sighup(int signo) {
+//     syslog(LOG_INFO, "Re-reading configuration file");
+// }
 
 
 int main(int argc, char *argv[]) {
