84e89e6c1bd4d4003de5e519dcd203dfef9def2a
Sek Cheong <scheong@gmail.com>
2019-09-23 01:52:32 -0500

    add

--- proj1/src/datastore.h
+++ proj1/src/datastore.h
@@ -4,17 +4,13 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include "sqlite3.h"
 #include "debug.h"
 #include "exception.h"
-#include "sqlite3.h"
 #include "sqlstatement.h"
-
 
 #define MAX_KEY_LEN 128
 #define MAX_VALUE_LEN 2048
-
-
-using value_p = std::shared_ptr<std::vector<char>>;
 
 int64_t get_timestamp();
 
@@ -47,48 +43,6 @@ public:
 		int len = strlen(value);
 		return put(key, value, len);
 	}
-
-	// virtual bool get(const char *key, std::vector<char> &value, int64_t &timestamp) {
-	// 	std::string s(key);
-	// 	return get(s, value, timestamp);
-	// }
-
-	// virtual bool get(std::string &key, std::vector<char> &value) {
-	// 	int64_t ts;
-	// 	return get(key, value, ts);
-	// }	
-	
-	// virtual bool put(const char *key, std::vector<char> &newvalue, std::vector<char> &oldvalue, int64_t &timestamp) {
-	// 	std::string s(key);
-	// 	return put(s, newvalue, oldvalue, timestamp);
-	// }
-
-	// virtual bool put(std::string &key, std::vector<char> &newvalue, std::vector<char> &oldvalue) {
-	// 	int64_t ts;
-	// 	return put(key, newvalue, oldvalue, ts);
-	// }
-
-	// virtual bool put(std::string &key, std::vector<char> &newvalue) {
-	// 	std::vector<char> oldvalue;
-	// 	return put(key, newvalue, oldvalue);		
-	// }
-	
-	// virtual bool put(const char *key, std::vector<char> &newvalue) {
-	// 	std::string s(key);
-	// 	return put(s, newvalue);
-	// }
-
-	// virtual bool put(const char *key, const char *value) {
-	// 	std::vector<char> data(value, value + strlen(value));
-	// 	std::string s(key);
-	// 	return put(s, data);
-	// }
-
-	// virtual bool put(const char *key, const char *value, std::vector<char> &oldvalue) {
-	// 	std::vector<char> data(value, value + strlen(value));
-	// 	std::string s(key);
-	// 	return put(s, data, oldvalue);
-	// }
 
 private:
 	std::string filename_;
